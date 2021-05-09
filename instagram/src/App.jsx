e698c934036f8344e8cc3caf8a2bb3aea40bde03
Hakkı Sağdıç <hakkisagdic@gmail.com>
2021-05-09 10:58:54 +0300

    signup page added awaiting fixes.

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/Signup'));
+// const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          {/* <Route path={ROUTES.NOT_FOUND} component={NotFound} /> */}
         </Switch>
       </Suspense>
     </Router>
---
+++ instagram/src/services/firebase.jsx
@@ -0,0 +1,8 @@
+import { firebase } from '../lib/firebase';
+
+export async function doesUsernameExist(username) {
+  const result = await firebase.firestore.collection('users').where('username', '==', username).get();
+  return result.docs.map((user) => user.data.length > 0);
+}
+
+export default doesUsernameExist;
